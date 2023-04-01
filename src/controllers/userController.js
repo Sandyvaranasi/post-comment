26879fa59b4d50f81dd45e6eba77cec0c6c873d4
Sandyvaranasi <119596263+Sandyvaranasi@users.noreply.github.com>
2023-04-01 15:35:28 +0530

    Update userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -8,28 +8,22 @@ const userLogin = async (req, res) => {
 
         if(Object.keys(data).length==0) return res.status(400).send("please provide name,email,phone and password");
 
-        let { name, email, password,...a} = data;
-
-        if(Object.keys(a).length!=0) return res.status(400).send("only name,email,phone and password is required");
+         let { name, email, password} = data;
 
         if (!email) return res.status(400).send("please provide email");
         if (!password) return res.status(400).send("please provide password");
         if (!name) return res.status(400).send("please provide name");
 
-
         if (!email.match(/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/)) return res.status(400).send("invalid email");
         if (!password.match(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,15}$/)) return res.status(400).send("invalid password");
         if (!name.match(/^[A-Z a-z]+$/)) return res.status(400).send("invalid name");
 
-        user = await userModel.findOne({email:email,password:password});
-        if(user){
-            let token = jwt.sign({ userId: user._id }, "very secret string");
-        return res.status(200).send({message:"ligin successful", token: token });
+        let user = await userModel.findOne({ email: email, password: password });
+        if (!user) {
+            user = await userModel.create(data);
         }
-         await userModel.create(data);
-        user = await userModel.findOne({email:email,password:password});
         let token = jwt.sign({ userId: user._id }, "very secret string");
-        return res.status(200).send({message:"ligin successful", token: token });
+        return res.status(200).send({ message: "ligin successful", token: token });
     } catch (err) {
         res.status(500).send(err.message);
     }
